dcbf06cf7bcb5412d6fb9c8d2f7986409e1df4ec
glaspal6 <dylan.r.kamstra@gmail.com>
2024-05-25 13:06:24 +0200

    feat(Hot reloading): Plug for hot reloading.
    Can change things in plug_update and then
    reload the dymanic library to show the
    changes.

--- src/main.c
+++ src/main.c
@@ -1,18 +1,77 @@
+#include <stddef.h>
+#include <dlfcn.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #include "raylib.h"
+#include "plug.h"
+
+const char *libplug_path = "libplug.so";
+void *libplug = NULL;
+
+bool reload_libplug()
+{
+    if (libplug != NULL) {
+        dlclose(libplug);
+    }
+
+    libplug = dlopen(libplug_path, RTLD_NOW);
+    if (libplug == NULL) {
+        fprintf(stderr, "ERROR: %s\n", dlerror());
+        return false;
+    }
+
+    plug_init = dlsym(libplug, "plug_init");
+    if (plug_init == NULL) {
+        fprintf(stderr, "ERROR: %s\n", dlerror());
+        return false;
+    }
+
+    plug_destroy = dlsym(libplug, "plug_destroy");
+    if (plug_destroy == NULL) {
+        fprintf(stderr, "ERROR: %s\n", dlerror());
+        return false;
+    }
+
+    plug_pre_reload = dlsym(libplug, "plug_pre_reload");
+    if (plug_pre_reload == NULL) {
+        fprintf(stderr, "ERROR: %s\n", dlerror());
+        return false;
+    }
+
+    plug_post_reload = dlsym(libplug, "plug_post_reload");
+    if (plug_post_reload == NULL) {
+        fprintf(stderr, "ERROR: %s\n", dlerror());
+        return false;
+    }
+
+    plug_update = dlsym(libplug, "plug_update");
+    if (plug_update == NULL) {
+        fprintf(stderr, "ERROR: %s\n", dlerror());
+        return false;
+    }
+
+    return true;
+}
 
 int main(void)
 {
+    if (!reload_libplug()) return 1;
+
+    plug_init();
     InitWindow(800, 600, "Hot Reloading");
 
     while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        EndDrawing();
+        if (IsKeyPressed(KEY_R)) {
+            void *state = plug_pre_reload();
+            reload_libplug();
+            plug_post_reload(state);
+        }
+        plug_update();
     }
 
     CloseWindow();
+    plug_destroy();
 
     return 0;
 }
---
+++ src/plug.h
@@ -0,0 +1,10 @@
+#ifndef PLUG_H
+#define PLUG_H
+
+void (*plug_init)(void) = NULL;
+void (*plug_destroy)(void) = NULL;
+void *(*plug_pre_reload)(void) = NULL;
+void (*plug_post_reload)(void *state) = NULL;
+void (*plug_update)(void) = NULL;
+
+#endif // !PLUG_H
