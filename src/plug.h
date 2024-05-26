6c94711a6cc1854396c13cf14a4f43d9160fa3a8
glaspal6 <dylan.r.kamstra@gmail.com>
2024-05-26 19:40:28 +0200

    feat(plug): Macro for the list of plugs

--- src/plug.h
+++ src/plug.h
@@ -3,10 +3,16 @@
 
 #include <stddef.h>
 
-void (*plug_init)(void) = NULL;
-void (*plug_destroy)(void) = NULL;
-void *(*plug_pre_reload)(void) = NULL;
-void (*plug_post_reload)(void *state) = NULL;
-void (*plug_update)(void) = NULL;
+#define LIST_OF_PLUGS \
+    PLUG((*plug_init), void, void) \
+    PLUG((*plug_destroy), void, void) \
+    PLUG(*(*plug_pre_reload), void , void) \
+    PLUG((*plug_post_reload), void, void *state) \
+    PLUG((*plug_update), void, void)
+
+#define PLUG(func, ret, arg) ret func(arg) = NULL;
+//LIST_OF_PLUGS
+LIST_OF_PLUGS
+#undef PLUG
 
 #endif // !PLUG_H
